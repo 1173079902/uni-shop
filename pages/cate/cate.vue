d969151b5c6d87cfa9e37e0fb4b68fde5ac9afa3
1173079902 <1173079902@qq.com>
2021-04-13 11:31:20 +0800

    15_获取分类列表的数据

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,139 +1,181 @@
 <template>
-	<view>
-	    <view class="scroll-view-container">
-	      <!-- 左侧的滚动视图区域 -->
-	      <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
-	        <view class="left-scroll-view-item active">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	        <view class="left-scroll-view-item">xxx</view>
-	      </scroll-view>
-	      <!-- 右侧的滚动视图区域 -->
-	      <scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}">
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	        <view class="left-scroll-view-item">zzz</view>
-	      </scroll-view>
-	    </view>
-	  </view>
+  <view>
+    <view class="scroll-view-container">
+      <!-- 左侧的滚动视图区域 -->
+      <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
+        <view class="left-scroll-view-item active">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+        <view class="left-scroll-view-item">xxx</view>
+      </scroll-view>
+      <!-- 右侧的滚动视图区域 -->
+      <scroll-view class="right-scroll-view" scroll-y :style="{height: wh + 'px'}">
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+        <view class="left-scroll-view-item">zzz</view>
+      </scroll-view>
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				wh: 0
-			};
-		},
+  export default {
+    data() {
+      return {
+        wh: 0,
+        // 分类数据列表
+        cateList: []
+      };
+    },
     onLoad() {
       const sysInfo = uni.getSystemInfoSync()
       this.wh = sysInfo.windowHeight
+      // 调用获取分类列表数据的方法
+      this.getCateList()
+    },
+    methods: {
+      async getCateList() {
+        // 发起请求
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/categories')
+        // 判断是否获取失败
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 转存数据
+        this.cateList = res.message
+      }
     }
-	}
+  }
 </script>
 
 <style lang="scss">
-.scroll-view-container {
-  display: flex;
-  
-  .left-scroll-view {
-    width: 120px;
+  .scroll-view-container {
+    display: flex;
+
+    .left-scroll-view {
+      width: 120px;
+
+      .left-scroll-view-item {
+        line-height: 60px;
+        background-color: #f7f7f7;
+        text-align: center;
+        font-size: 12px;
+
+        // 激活项的样式
+        &.active {
+          background-color: #ffffff;
+          position: relative;
+
+          // 渲染激活项左侧的红色指示边线
+          &::before {
+            content: ' ';
+            display: block;
+            width: 3px;
+            height: 30px;
+            background-color: #c00000;
+            position: absolute;
+            left: 0;
+            top: 50%;
+            transform: translateY(-50%);
+          }
+        }
+      }
+    }
   }
-}
 </style>
