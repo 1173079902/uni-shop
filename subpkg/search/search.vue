669085b106661d172c6e8b2d89c49f6fcbfdf432
1173079902 <1173079902@qq.com>
2021-04-14 10:31:45 +0800

    29_渲染搜索历史的结构并美化

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -9,6 +9,18 @@
         <uni-icons type="arrowright" size="16"></uni-icons>
       </view>
     </view>
+    <!-- 搜索历史 -->
+    <view class="history-box">
+      <!-- 标题区域 -->
+      <view class="history-title">
+        <text>搜索历史</text>
+        <uni-icons type="trash" size="17"></uni-icons>
+      </view>
+      <!-- 列表区域 -->
+      <view class="history-list">
+        <uni-tag :text="item" v-for="(item, i) in historyList" :key="i"></uni-tag>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -21,7 +33,9 @@
         // 搜索关键词
         kw: '',
         // 搜索结果列表
-        searchResults: []
+        searchResults: [],
+        // 搜索关键词的历史记录
+        historyList: ['a', 'app', 'apple']
       };
     },
     methods: {
@@ -76,6 +90,28 @@
         // 文本溢出后，使用 ... 代替
         text-overflow: ellipsis;
         margin-right: 3px;
+      }
+    }
+  }
+  .history-box {
+    padding: 0 5px;
+  
+    .history-title {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 40px;
+      font-size: 13px;
+      border-bottom: 1px solid #efefef;
+    }
+  
+    .history-list {
+      display: flex;
+      flex-wrap: wrap;
+  
+      .uni-tag {
+        margin-top: 5px;
+        margin-right: 5px;
       }
     }
   }
